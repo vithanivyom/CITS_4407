217b82d4eeca8a8b07c895e455d2538d7a438f20
vyom <vithanivyom2@gmail.com>
2024-05-18 09:48:41 +0800

    fixed the bug regarding file 3

--- cantril_data_cleaning.sh
+++ cantril_data_cleaning.sh
@@ -41,15 +41,18 @@ awk -F'\t' -v OFS='\t' '
         a[$1 FS $3] = $0
         next
     }
-    # For the second and third files, if key exists in array a, print combined lines
     ($1 FS $3) in a {
         print a[$1 FS $3], $0
     }
-' "$input_file1" "$input_file2" "$input_file3" |
+' "$input_file1" "$input_file2" | awk '
+    BEGIN {FS=OFS="\t"}
+    FNR==NR {b[$1 FS $3] = $0; next}
+    # For the second and third files, if key exists in array a, print combined lines
+    ($1 FS $3) in b {print $0, b[$1 FS $3]}
+' - "$input_file3" |
 awk -F'\t' -v OFS='\t' '
     # Print header line and filter data based on conditions
     NR == 1 || ($3 >= 2011 && $3 <= 2021 && $2 != "") {
-        print $1, $2, $3, $12, $6, $18, $4, $5
+        print $1,$2,$3,$12,$6,$18,$4,$5
     }
 ' > "cantril_data_cleaning.tsv"
-
